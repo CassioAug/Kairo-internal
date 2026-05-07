d34aebb41a38d4f7257e87a85b67394a14346d78
Edward Kemp <edwardlanekemp@gmail.com>
2026-05-07 13:04:59 +0100

    Update Kairo app label

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -96,7 +96,7 @@ android {
         applicationId = "com.kairo.reader"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
