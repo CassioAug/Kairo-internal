7b1bc1d85fcf3789f9e9caac17044eadd8a2c25c
Edward Kemp <edwardlanekemp@gmail.com>
2026-07-25 19:36:45 +0100

    build: migrate to AGP 9 toolchain

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,6 @@ import java.util.Properties
 
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.compose.compiler)
     alias(libs.plugins.detekt)
     alias(libs.plugins.ktlint)
@@ -130,9 +129,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
