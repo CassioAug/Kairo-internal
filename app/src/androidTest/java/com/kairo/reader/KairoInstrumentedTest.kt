c6e1c1e1d02f0953a377e09e6d16237eb3eb8852
Edward Kemp <edwardlanekemp@gmail.com>
2026-08-09 21:18:23 +0100

    build: target Android 16

--- app/src/androidTest/java/com/kairo/reader/KairoInstrumentedTest.kt
+++ app/src/androidTest/java/com/kairo/reader/KairoInstrumentedTest.kt
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith
 
 private const val DEBUG_APPLICATION_ID = "com.kairo.reader.debug"
 private const val DEBUG_APPLICATION_LABEL = "Kairo Dev"
+private const val EXPECTED_TARGET_SDK = 36
 
 @RunWith(AndroidJUnit4::class)
 class KairoInstrumentedTest {
@@ -19,5 +20,6 @@ class KairoInstrumentedTest {
 
         assertEquals(DEBUG_APPLICATION_ID, appContext.packageName)
         assertEquals(DEBUG_APPLICATION_LABEL, applicationLabel)
+        assertEquals(EXPECTED_TARGET_SDK, appContext.applicationInfo.targetSdkVersion)
     }
 }
